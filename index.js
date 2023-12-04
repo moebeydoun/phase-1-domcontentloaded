a183f078fd6787902a03060fd7f43ca6766d13e5
moe beydoun <moebeydoun24@yahoo.com>
2023-12-04 16:22:01 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Wait for the DOM content to be fully loaded
+
+    // Find the element by its ID
+    const myElement = document.getElementById('myElementId');
+
+    // Change the text content
+    myElement.textContent = 'This is really cool!';
+});
